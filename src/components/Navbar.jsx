6265d80df40eb214170814fb65af1aac30dcfd21
Rebaiahmed <ahmed.bouhmid94@gmail.com>
2020-02-08 16:06:05 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Navbar() {
+    return <React.Fragment>
+
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Study-tn React App</a>
+        </nav>
+    </React.Fragment>;
+}
+
+export default Navbar;  
---
+++ src/components/Todos.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+
+class Todos extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      msg: '',
+      data: [
+        {
+          id: 0,
+          "title": "Finish my React course",
+          "status": false
+        },
+        {
+          id: 1,
+          "title": "Share Study React course",
+          "status": false
+        },
+        {
+          id: 2,
+          "title": "Deploy my React App",
+          "status": false
+        }
+      ]
+    }
+  }
+
+
+  componentDidMount() {
+    setTimeout(() => {
+      console.log('Our data is fetched');
+      this.setState({
+      })
+    }, 1000)
+
+  }
+
+  deleteTodo = (id,event) => {
+    alert("event"+event.type);
+    let { data } = this.state;
+    data.splice(id, 1);
+    this.setState({
+      data: data
+    });
+  }
+
+  render() {
+    let { data, msg } = this.state;
+    return (
+      <React.Fragment>
+        <h3>{msg}</h3>
+        <ul className="list-group">
+          {data.map((value, index) => {
+            return <TodoItem deleteTodo={this.deleteTodo} id={value.id} title={value.title} index={index} date={new Date()} />
+          })}
+        </ul>
+
+      </React.Fragment>
+    )
+  }
+}
+
+
+
+export default Todos;
