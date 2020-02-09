0791d6a0217dc61819510d022c090c17e002370e
Rebaiahmed <ahmed.bouhmid94@gmail.com>
2020-02-09 01:39:04 +0100

    Updates

---
+++ precache-manifest.e2c44815282f3aa1da99e4cb22003875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f3cd59097629790afa1caf197d0d3b",
+    "url": "/study-tn-react-app/index.html"
+  },
+  {
+    "revision": "500ceef1ecc31a0e9d96",
+    "url": "/study-tn-react-app/static/css/main.299b5a05.chunk.css"
+  },
+  {
+    "revision": "5ba30f3bc8b0abc2f208",
+    "url": "/study-tn-react-app/static/js/2.6eaf9fae.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/study-tn-react-app/static/js/2.6eaf9fae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "500ceef1ecc31a0e9d96",
+    "url": "/study-tn-react-app/static/js/main.40b24e20.chunk.js"
+  },
+  {
+    "revision": "820a571463a69cb25cc2",
+    "url": "/study-tn-react-app/static/js/runtime-main.e3a8584b.js"
+  }
+]);
